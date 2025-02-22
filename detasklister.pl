035eff1cd7b6e792099830348a0f3080a057f443
Adam Curtis <adam.curtis.dev@gmail.com>
2025-02-22 15:18:00 +1300

    feat: usability improvements and bug fixes (see long msg)
    * Added help for interactive mode (type '?')
    * More clear indication that displayed commands are not being executed
      in --dry_run mode
    * Added -h option
    * Added --suppress-unchanged option
    * Added --context option
    * Improvements and fixes to command-line options validation
    * Changed --issue-state option to "all" by default
    * Better terminal detection and NO_COLOR support
    * Fixed issues with writing UTF-8 characters to generated tmp files
    * Fixed output flushing behavior when running in interactive mode and
      output is being piped to another process (such as with `tee`)
    * Better formatting of temp file names to indicate issue number and
      purpose
    * Improved efficiency of regex when finding context for diffs
    * Added docs for helper/utility functions
    * Added 'ignore_error' keyword argument to `run_cmd` function to ignore
      exit status
    * New utility function `write_temp` for creating tmp files
    * Simplified repo name parsing logic
    * Various fixes/improvements to perl syntax and feature usage to
      eliminate sources of bugs

--- detasklister.pl
+++ detasklister.pl
@@ -12,29 +12,36 @@ detasklister [options] [<issue number or url> ...]
     -R, --repo [HOST/]/OWNER/REPO           Selects a GitHub repo to change
     -i, --interactive                       Interactive mode, prompt for each change
     -A, --all-issues                        Modify all issues in a repo 
-    -s, --issue-state {open|closed|all}     Filter by issue state when using --all-issues (default: open)
+    -s, --issue-state {open|closed|all}     Filter by issue state when using --all-issues (default: all)
     -n, --dry-run                           Show changes without performing them
     -v, --verbose                           Verbose output
         --debug                             Debugging output
-        --help                              Show help message 
+        --suppress-unchanged                Do not show any output from unchanged issues
+    -C  --context int                       Display N lines of context in interactive diffs (default: 5)
+    -h, --help                              Show help message 
 =cut
 
 use strict;
+use warnings;
 use v5.20;
+use utf8;
 
-use Getopt::Long qw(:config gnu_getopt auto_abbrev auto_help);
+use Getopt::Long qw(:config gnu_getopt auto_abbrev);
 use JSON::PP;
 use File::Temp qw(tempfile);
 use File::Spec;
 use Pod::Usage;
 
-my $tmpdir = File::Spec->tmpdir() or '/tmp'; # Fixes MacOS weirdness with TMPDIR
-my $use_color = not $ENV{NO_COLOR};          # https://no-color.org/
+### Constants
+
+my $use_color =
+  !( defined $ENV{NO_COLOR} && $ENV{NO_COLOR} ne '' ) && -t select;
+my @issue_state_options = qw(open closed all);
 
 ### Variables for storing command-line options
-my $host;
-my $owner;
+
 my $repo;
+my %repo_parts;    # Hash of host,owner,repo parts
 my $interactive;
 my $all_issues;
 my $issue_state;
@@ -44,19 +51,36 @@ my $dry_run;
 #my $comments;
 my $verbose;
 my $debug;
+my $suppress_unchanged;
+my $num_context;
 
 ### Utility functions
+
+# Run a shell command and return the output. Respects command-line logging options.
+#
+# Keyword arguments:
+#   print_output    if true, always print output. if false, never print output.
+#                   By default, print on error or when --debug flag is used.
+#
+#   ignore_error    by default, an error code will exit the program.
+#                   Set this flag to ignore the exit code.
 sub run_cmd {
     my ( $cmd, %args ) = @_;
-    say ">>> $cmd" if $verbose or $debug;
+    say ">>> $cmd" if $verbose || $debug;
     my $output = `$cmd`;
     $output =~ s/\n?$/\n/
       if $output ne '';    # ensure trailing newline in command output
-    print $output if $debug or $args{print} or $?;
-    exit $?       if $?;
+    print $output if $debug || $args{print_output} || $?;
+    exit $?       if $? && !$args{ignore_error};
     return $output;
 }
 
+# Escape shell argument with single quotes.
+#
+# Example argument:
+#   don't worry be happy
+# Returns:
+#   'don'"'"'t worry be happy'
 sub shell_escape {
     foreach (@_) {
         s/'/'"'"'/g;
@@ -65,94 +89,144 @@ sub shell_escape {
     return @_;
 }
 
+# Create and write to a temp file
+sub write_temp {
+    my ( $name, $contents, %args ) = @_;
+    state $tmpdir = File::Spec->tmpdir();
+    $args{DIR} = $tmpdir unless $args{DIR};
+    my ( $fh, $fname ) = tempfile( "detasklister.$name.XXXXXXXXXX", %args );
+    binmode( $fh, ":utf8" );
+    print $fh $contents;
+    return ( $fname, $fh );
+}
+
+# Display a diff to the user with the system `diff` command.
 sub show_diff {
-    my ( $old, $new ) = @_;
-    my ( $old_file, $old_name ) =
-      tempfile( "detasklister.old.XXXXXXXXXX", DIR => $tmpdir );
-    my ( $new_file, $new_name ) =
-      tempfile( "detasklister.new.XXXXXXXXXX", DIR => $tmpdir );
-    print $old_file $old;
-    print $new_file $new;
-    my $color_opt = $use_color ? '--color=always' : '--color=never';
-    run_cmd( "diff -u $color_opt $old_name $new_name || :", print => 1 );
+    my ( $old, $new, %args ) = @_;
+    $args{name} = "diff" unless defined $args{name};
+    my ($old_name) = write_temp( "$args{name}.old", $old );
+    my ($new_name) = write_temp( "$args{name}.new", $new );
+    my $color_opt  = $use_color ? '--color=always' : '--color=never';
+    run_cmd(
+        "diff -u $color_opt $old_name $new_name",
+        print_output => 1,
+        ignore_error => 1
+    );
 }
 
-unless ( @ARGV ) {
-    pod2usage(1);
-}
-
-GetOptions(
-    "repo|R=s" => sub {
-
-        ## Validate and parses repo string format
-        my ( $opt_name, $opt_value ) = @_;
-        $opt_value =~ m'^
+# Parses the [HOST/]OWNER/REPO format
+sub parse_repo_opt {
+    my ( $opt_name, $opt_value ) = @_;
+    $opt_value =~ m'^
             (?:(?<host>[^/]+)/)?
             (?<owner>[^/]+)/
             (?<repo>[^/]+)
         $'x
-          or die "Expected the '[HOST]/OWNER/REPO' format, got '$opt_value'\n";
-        ( $host, $owner, $repo ) = @+{ 'host', 'owner', 'repo' };
-    },
-    "interactive|i"   => \$interactive,
-    "all-issues|A"    => \$all_issues,
-    "issue-state|s=s" => \$issue_state,
-    "verbose|v"       => \$verbose,
-    "debug"           => \$debug,
-    "dry-run|n"       => \$dry_run,
+      or die "Expected the '[HOST]/OWNER/REPO' format, got '$opt_value'\n";
+    %repo_parts = %+;
+    $repo       = $opt_value;
+}
+
+# Parse an issue argument string and returns the number as a string
+sub parse_issue {
+    ($_) = @_ if @_;
+    return $1 if m'^(?|#?(\d+)|https?://.+?/.+?/.+?/issues/(\d+)/?)$';
+}
+
+### Show help when there are no arguments
+
+pod2usage(1) unless @ARGV;
+
+### Parse command-line options
+
+GetOptions(
+    "repo|R=s"           => \&parse_repo_opt,
+    "interactive|i"      => \$interactive,
+    "all-issues|A"       => \$all_issues,
+    "issue-state|s=s"    => \$issue_state,
+    "dry-run|n"          => \$dry_run,
+    "verbose|v"          => \$verbose,
+    "debug"              => \$debug,
+    "suppress-unchanged" => \$suppress_unchanged,
+    "context|C=i"        => \$num_context,
+    "help|h"             => sub { podusage(1); },
 ) or exit 1;
 
-## Validate argument combinations
-if ($all_issues) {
+### Validate command-line arguments
+
+if ( defined $all_issues ) {
     if (@ARGV) {
         die "Cannot combine positional arguments with --all-issues (-A)\n";
     }
-    if ( not $repo ) {
+    if ( !defined $repo ) {
         die "--repo (-R) is required when using --all-issues (-A)\n";
     }
-    if(not defined $issue_state) {
-        $issue_state = 'open';
-    }
-    if ( not grep /^\Q$issue_state\E$/, 'all', 'open', 'closed' ) {
-        die "--issue-state (-s) must be one of 'all', 'open', or 'closed'\n";
-    }
-}
-else {
-    if ($issue_state) {
-        die "--issue-state (-s) requires --all-issues (-A)\n";
+    if ( defined $issue_state
+        && !grep( /^\Q$issue_state\E$/, @issue_state_options ) )
+    {
+        die '--issue-state (-s) must be one of '
+          . join( ', ', @issue_state_options ) . "\n";
     }
 }
+elsif ( defined $issue_state ) {
+    die "--issue-state (-s) requires --all-issues (-A)\n";
+}
 
-## construct --json flag for `gh issue view` command
-my @json_fields = ( 'url', 'body' );
-my $json_opt    = "--json '@{[join(',', @json_fields )]}'";
+if ( !defined $interactive && defined $num_context ) {
+    die "--context (-C) can only used with --interactive (-i)\n";
+}
+if ( defined $num_context && $num_context < 0 ) {
+    die "--context (-C) must be a non-negative number\n";
+}
 
-## construct --repo flag for `gh issue view` and `gh issue list` commands
-my $repo_str = join( '/', grep( !/^$/, $host, $owner, $repo ) );
-my $repo_opt = $repo_str ? "--repo @{[ shell_escape $repo_str ]}" : '';
-
+# Validate issue format. Should be either issue number or an issue URL.
 foreach (@ARGV) {
-    ## Validate issue format. Should be either issue number or an issue URL.
-    m'^(#?\d+|https?://.+?/.+?/.+?/issues/\d+)$'
-      or die "Invalid issue format '$_'\n";
+    parse_issue or die "Invalid issue format '$_'\n";
 }
+
+### Default option values
+
+$issue_state = 'all' if !defined $issue_state;
+$num_context = 5     if !defined $num_context;
+
+### Autoflush STDOUT in interactive mode
+
+# Prevents commands like `tee` from interfering with flush behavior
+STDOUT->autoflush(1) if $interactive;
+
+### Prepare common GH CLI options
+
+# construct --repo flag for `gh issue view` and `gh issue list` commands
+my $repo_opt = $repo ? "--repo @{[ shell_escape $repo ]}" : '';
+
+### Prepare the list of issues to read/edit
 
 my @issue_names;
-if ( not $all_issues ) {
+if ( !$all_issues ) {
     @issue_names = @ARGV;
 }
 else {
-    my $cmd =
-"gh issue list $repo_opt --json url --state $issue_state --limit 2147483647";
+    # options for `gh issue list`
+    my $list_opts =
+      "$repo_opt --json url --state $issue_state --limit 2147483647";
+    my $cmd  = "gh issue list $list_opts";
     my $json = decode_json( run_cmd($cmd) );
     @issue_names = map { $_->{url} } @$json;
 }
 
+### Main view/edit loop
+
 foreach my $issue_name (@issue_names) {
     ## Fetch the issues with GH CLI
-    my $issue = decode_json(
-        run_cmd("gh issue view --json 'url,body' $repo_opt '$issue_name'") );
-
+    my $issue_number = parse_issue $issue_name;
+    my $view_opts    = "--json 'url,body'";
+    if ($repo_opt) {
+        $view_opts .= " $repo_opt '$issue_number'";
+    }
+    else {
+        $view_opts .= " '$issue_name'";
+    }
+    my $issue = decode_json( run_cmd("gh issue view $view_opts") );
     ## Search/Replace tasklist blocks
     my $body    = $issue->{body};
     my $changes = $body;
@@ -169,41 +243,83 @@ foreach my $issue_name (@issue_names) {
       )
     {
         my ( $outer, $inner ) = @+{ 'outer', 'inner' };
-        my ($context) = $body =~ /(
-            (?:.*?\n){0,5}
-            \Q$outer\E
-            (?:.*?\n){0,5}
-        )/xs;
-        my $choice;
-        if ( $interactive and not $yes_to_all ) {
-            my $context_changes = ( $context =~ s/\Q$outer\E/$inner/r );
-            show_diff( $context, $context_changes );
-            my $input;
-            do {
-                print "\nRemove this tasklist block [y/n/a/d/q]? ";
-                $input = <STDIN>;
-            } until ($choice) = ( $input =~ /^ *([ynadq]) *$/i );
 
+        # Grab the context around the tasklist
+        # This is done as a separate regex to avoid messing up the looping
+        # behavior of the /g modifier when tasklists appear in the sliding
+        # context window
+        my ($context) = $body =~ /(
+            (?:\N*\n){0,$num_context}
+            \Q$outer\E
+            (?:\N*\n){0,$num_context}
+        )/xms;
+
+        # non-interactive mode
+        if ( !$interactive || $yes_to_all ) {
+            $changes =~ s/\Q$outer\E/$inner/;
+        }
+
+        # interactive mode
+        else {
+            my $choice;
+
+            # apply the change to the context area and display as a diff to the
+            # user
+            my $context_changes = ( $context =~ s/\Q$outer\E/$inner/r );
+            show_diff( $context, $context_changes,
+                name => "issue.$issue_number.tasklist" );
+
+            # prompt for input
+            my $input;
+          PROMPT:
+            do {
+                print "\nRemove this tasklist block [y/n/a/d/q/?]? ";
+                $input = <STDIN>;
+            } until ($choice) = ( $input =~ /^ *([ynadq?]) *$/i );
+
+            # process the user's input
             $choice = lc($choice);
-            if    ( $choice eq 'a' ) { $yes_to_all = 1; }
+            if    ( $choice eq 'y' ) { $changes =~ s/\Q$outer\E/$inner/ }
+            elsif ( $choice eq 'n' ) { next TASKLIST_IN_ISSUE; }
+            elsif ( $choice eq 'a' ) { $yes_to_all = 1; }
             elsif ( $choice eq 'd' ) { last TASKLIST_IN_ISSUE; }
             elsif ( $choice eq 'q' ) { die "Quit\n"; }
-        }
-        if ( not $interactive or $yes_to_all or $choice eq 'y' ) {
-            $changes =~ s/\Q$outer\E/$inner/;
+            elsif ( $choice eq '?' ) {
+                print <<~'END';
+                    y - stage this change
+                    n - do not stage this change
+                    q - quit; do not stage this change or any remaining ones
+                    a - stage this change and all later changes in this issue
+                    d - do not stage this change or any of the later changes in this issue
+                    ? - print help
+                END
+                goto PROMPT;    # mama mia that's some good spaghetti
+            }
+            else {
+                die <<~END;
+                    UNEXPECTED ERROR: received 'impossible' choice '$choice'
+
+                    Please report this as a bug :)
+                END
+            }
         }
     }
     if ( $body eq $changes ) {
-        say "No changes to make for $issue_name";
+        say "No changes to make for $issue_name" if !$suppress_unchanged;
         next;
     }
-    my ( $changes_file, $changes_name ) =
-      tempfile( "detasklister.changes.XXXXXXXXXX", DIR => $tmpdir );
-    print $changes_file $changes;
-    my $cmd = "gh issue edit '$issue_name' --body-file '$changes_name'";
+    my ($changes_name) = write_temp( "issue.$issue_number.changes", $changes );
+    my $edit_opts = "--body-file '$changes_name'";
+    if ($repo_opt) {
+        $edit_opts .= " $repo_opt '$issue_number'";
+    }
+    else {
+        $edit_opts .= " '$issue_name'";
+    }
+    my $cmd = "gh issue edit $edit_opts";
     if ($dry_run) {
-        say ">>> $cmd";
-        show_diff( $body, $changes );
+        say "DRY RUN >>> $cmd";
+        show_diff( $body, $changes, name => "issue.$issue_number" );
         next;
     }
     say "Updating $issue_name...";
